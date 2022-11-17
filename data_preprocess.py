681d506a71e7c0e3aeee9f65c99efdc5800c6a0a
lbidulka <lukebidulka@gmail.com>
2022-11-16 23:35:33 -0800

    one-hot label encoding

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,6 +2,7 @@ import torch
 import tfrecord
 import os
 import numpy as np
+from tqdm import tqdm
 
 train_bal_path = "./data/audioset_features/audioset_v1_embeddings/bal_train/" 
 eval_path = "./data/audioset_features/audioset_v1_embeddings/eval/" 
@@ -12,7 +13,7 @@ train_bal_outpath = "./data/audioset_features/audioset_music_only/bal_train_"
 eval_outpath = "./data/audioset_features/audioset_music_only/eval_" 
 
 genre_indices = [216, 217, 219, # Pop, Hip Hop, Rock
-                 226, 228, 227, # R&B, Reggae, Soul 
+                 226, 227, 228, # R&B, Soul, Reggae
                  229, 232, 233, # Country, Funk, Folk
                  234, 235, 236, # Middle Eastern, Jazz, Disco
                  237, 239, 248, # Classical, Electronic, Latin
@@ -24,15 +25,15 @@ mood_indices = [276, 277, 278, # Happy, Funny, Sad
                 279, 280, 281, # Tender, Exciting, Angry
                 282] # Scary
 
-genre_samples = []
-genre_samples_labels = []
-mood_samples = []
-mood_samples_labels = []
-
 print("Diving in...")
 for in_path, out_path in zip([train_bal_path, eval_path], [train_bal_outpath, eval_outpath,]):
     print("\nWorking on: ", in_path)
-    for filename in os.listdir(in_path):
+    genre_samples = []
+    genre_samples_labels = []
+    mood_samples = []
+    mood_samples_labels = []
+
+    for filename in tqdm(os.listdir(in_path)):
         fp = os.path.join(in_path, filename)
 
         if os.path.isfile(fp):
@@ -47,11 +48,14 @@ for in_path, out_path in zip([train_bal_path, eval_path], [train_bal_outpath, ev
                 labels = context["labels"]
 
                 if set(labels) & set(genre_indices):
+                    # print("Labels: ", labels)
                     # Get the genre labels only
                     pruned_labels = []
-                    for label in labels:
-                        if label in genre_indices:
-                            pruned_labels.append(label)
+                    for idx in genre_indices:
+                        if idx in labels:
+                            pruned_labels.append(1)
+                        else:
+                            pruned_labels.append(0)
 
                     # Get the feature samples, and multiply the labels accordingly
                     for feat in data:
@@ -59,11 +63,13 @@ for in_path, out_path in zip([train_bal_path, eval_path], [train_bal_outpath, ev
                         genre_samples_labels.append(pruned_labels)
                 
                 if set(labels) & set(mood_indices):
-                    # Get the genre labels only
+                    # Get the mood labels only
                     pruned_labels = []
-                    for label in labels:
-                        if label in mood_indices:
-                            pruned_labels.append(label)
+                    for idx in mood_indices:
+                        if idx in labels:
+                            pruned_labels.append(1)
+                        else:
+                            pruned_labels.append(0)
 
                     # Get the feature samples, and multiply the labels accordingly
                     for feat in data:
@@ -83,5 +89,8 @@ for in_path, out_path in zip([train_bal_path, eval_path], [train_bal_outpath, ev
     mood_samples_arr = np.array(mood_samples)
     np.save(out_path + "mood_labels.npy", mood_samples_labels_arr)
     np.save(out_path + "mood_data.npy", mood_samples_arr)
+
+    # print("genre labels: ", genre_samples_labels_arr.shape)
+    # print("mood labels: ", mood_samples_labels_arr.shape)
 
 print("Complete!")
