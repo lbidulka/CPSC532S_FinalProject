4e4a369931840dcfa85adb5a071d46f3821754ec
lbidulka <lukebidulka@gmail.com>
2022-12-08 15:58:53 -0800

    prompt extender, multiple outputs, album vs concept output, refactoring

--- full_inference.py
+++ full_inference.py
@@ -4,10 +4,11 @@ from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 from transformers import pipeline
 import lovely_tensors as lt
 lt.monkey_patch()
+import json
 
 from utils.models import genre_classifier, mood_classifier
-from utils.dataset import AudioSet, genre_indices, mood_indices
-from utils.prompt_templates import genre_prompts, moods, genres
+from utils.dataset import AudioSet, genre_indices, mood_indices, moods, genres
+# from utils.label_info import moods, genres
 
 # SEED = 1234
 # np.random.seed(SEED)
@@ -31,7 +32,8 @@ def main():
     vid_url = ""
     for id in vid_id[0].tolist():
         vid_url += chr(int(id))
-    print("Input video ID: https://www.youtube.com/watch?v={}{}".format(vid_url, "&t=" + str(int(times[0,0].item())) + "s"))
+    vid_url = "https://www.youtube.com/watch?v=" + vid_url + "&t=" + str(int(times[0,0].item())) + "s"
+    print("Input video URL: ", vid_url)
     print("Time from {}s to {}s\n".format(times[0,0].item(), times[0,1].item()))
 
     mood_idx = torch.argmax(mood)
@@ -41,36 +43,47 @@ def main():
     # Get mood/genre of feature
     pred_mood = mood_model(feature)
     pred_genre = genre_model(feature)
-
     pred_mood_idx = torch.argmax(pred_mood).item()
     pred_genre_idx = torch.argmax(pred_genre).item()
-    pred_mood_audioset_idx = mood_indices[pred_mood_idx]
-    pred_genre_audioset_idx = genre_indices[pred_genre_idx]
-
     print("GT mood: ", moods[mood_idx], ", Pred mood: ", moods[pred_mood_idx])
     print("GT genre: ", genres[genre_idx], ", Pred genre: ", genres[pred_genre_idx], "\n")
     
     # Create genre-based prompt
-    SD_input_mood = moods[pred_mood_idx]
-    # SD_prompt = genre_prompts[pred_genre_idx].replace("*", SD_input_mood)   # Get prompt from our list, add in the mood
-    SD_prompt = "Really " + moods[pred_mood_idx] + " " + genres[pred_genre_idx] + " music album cover"
+    SD_prompt = moods[pred_mood_idx] + " " + genres[pred_genre_idx] + " music album cover"
     print("Input prompt: ", SD_prompt, "\n")
-
-    # Extend prompt
     text_pipe = pipeline('text-generation', model='daspartho/prompt-extend', device=0)
-    extended_SD_prompt = text_pipe(SD_prompt+',', num_return_sequences=1, max_length=50)[0]["generated_text"]
+    extended_SD_prompt = text_pipe(SD_prompt+',', num_return_sequences=1, max_length=25)[0]["generated_text"]
     extended_SD_prompt
     print("Extended input prompt: ", extended_SD_prompt, "\n")
 
-    # Generate output img with SD
+    # Generate output imgs with SD
+    num_out_imgs = 2
     SD_model_id = "stabilityai/stable-diffusion-2"
     scheduler = EulerDiscreteScheduler.from_pretrained(SD_model_id, subfolder="scheduler")
     pipe = StableDiffusionPipeline.from_pretrained(SD_model_id, scheduler=scheduler, torch_dtype=torch.float16, revision="fp16")
     pipe = pipe.to("cuda")
     pipe.enable_attention_slicing() # Reduce GPU usage
-    SD_images = pipe(SD_prompt).images
-    print("Created img. Saving...")
-    SD_images[0].save("./CPSC532S_FinalProject/outputs/output.png")
+    SD_images_album = pipe([SD_prompt] * num_out_imgs).images             # Album cover
+    SD_images_concept = pipe([extended_SD_prompt] * num_out_imgs).images    # Concept art
+    
+    # Dump outputs
+    out_dir = "./CPSC532S_FinalProject/outputs/"
+    info_dict = {
+        'URL': vid_url,
+        'GT_mood': moods[mood_idx],
+        'GT_genre': genres[genre_idx],
+        'pred_mood': moods[pred_mood_idx], 
+        'pred_genre': genres[pred_genre_idx],
+        'album_prompt': SD_prompt,
+        'concept_prompt': extended_SD_prompt,
+    }
+    info_json = json.dumps(info_dict)
+    with open(out_dir + "info.json", "w") as outfile:
+        outfile.write(info_json)
+    for i, img in enumerate(SD_images_album):
+        img.save(out_dir + "album_output_" + str(i) + ".png")
+    for i, img in enumerate(SD_images_concept):
+        img.save(out_dir + "concept_output_" + str(i) + ".png")
 
 if __name__ == "__main__":
     main()
--- utils/dataset.py
+++ utils/dataset.py
@@ -17,6 +17,18 @@ mood_indices = [276, 277, 278, # Happy, Funny, Sad
                 279, 280, 281, # Tender, Exciting, Angry
                 282] # Scary
 
+moods = ["Happy", "Funny", "Sad", "Tender", "Exciting", "Angry", "Scary"]
+
+genres = ["Pop", "Hip Hop", "Rock",
+            "R&B", "Soul", "Reggae",
+            "Country", "Funk", "Folk",
+            "Middle Eastern", "Jazz", "Disco",
+            "Classical", "Electronic", "Latin",
+            "Blues", "Chilren's", "New-age",
+            "Vocal", "Music of Africa", "Christian",
+            "Music of Asia", "Ska", "Traditional",
+            "Independent"]
+
 class AudioSet():
     def __init__(self, batch_size = 4096, inference = False) -> None:
         self.batch_size = batch_size
