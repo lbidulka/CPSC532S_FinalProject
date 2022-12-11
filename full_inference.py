006ec547a7ce65f3d97fa2d303d95f9a5f7a6b8a
lbidulka <lukebidulka@gmail.com>
2022-12-11 12:00:45 -0800

    fixed multi-label consideration

--- full_inference.py
+++ full_inference.py
@@ -36,54 +36,69 @@ def main():
     print("Input video URL: ", vid_url)
     print("Time from {}s to {}s\n".format(times[0,0].item(), times[0,1].item()))
 
-    mood_idx = torch.argmax(mood)
-    genre_idx = torch.argmax(genre)
+    mood_idxs = torch.nonzero(mood.squeeze()).reshape(-1,).tolist()
+    genre_idxs = torch.nonzero(genre.squeeze()).reshape(-1,).tolist()
+    GT_moods = [moods[idx] for idx in mood_idxs]
+    GT_genres = [genres[idx] for idx in genre_idxs]
+
     feature = feature.to(device)
 
     # Get mood/genre of feature
+    NUM_MOOD_PREDS = 2
+    NUM_GENRE_PREDS = 3
+    
     pred_mood = mood_model(feature)
     pred_genre = genre_model(feature)
-    pred_mood_idx = torch.argmax(pred_mood).item()
-    pred_genre_idx = torch.argmax(pred_genre).item()
-    print("GT mood: ", moods[mood_idx], ", Pred mood: ", moods[pred_mood_idx])
-    print("GT genre: ", genres[genre_idx], ", Pred genre: ", genres[pred_genre_idx], "\n")
-    
-    # Create genre-based prompt
-    SD_prompt = moods[pred_mood_idx] + " " + genres[pred_genre_idx] + " music album cover"
-    print("Input prompt: ", SD_prompt, "\n")
-    text_pipe = pipeline('text-generation', model='daspartho/prompt-extend', device=0)
-    extended_SD_prompt = text_pipe(SD_prompt+',', num_return_sequences=1, max_length=25)[0]["generated_text"]
-    extended_SD_prompt
-    print("Extended input prompt: ", extended_SD_prompt, "\n")
+    _, pred_mood_idxs = torch.topk(pred_mood, NUM_MOOD_PREDS)
+    _, pred_genre_idxs = torch.topk(pred_genre, NUM_GENRE_PREDS)
+    pred_mood_idxs = pred_mood_idxs.reshape(-1,).tolist()
+    pred_genre_idxs = pred_genre_idxs.reshape(-1,).tolist()
+    pred_moods = [moods[idx] for idx in pred_mood_idxs]
+    pred_genres = [genres[idx] for idx in pred_genre_idxs]
 
-    # Generate output imgs with SD
-    num_out_imgs = 2
-    SD_model_id = "stabilityai/stable-diffusion-2"
-    scheduler = EulerDiscreteScheduler.from_pretrained(SD_model_id, subfolder="scheduler")
-    pipe = StableDiffusionPipeline.from_pretrained(SD_model_id, scheduler=scheduler, torch_dtype=torch.float16, revision="fp16")
-    pipe = pipe.to("cuda")
-    pipe.enable_attention_slicing() # Reduce GPU usage
-    SD_images_album = pipe([SD_prompt] * num_out_imgs).images             # Album cover
-    SD_images_concept = pipe([extended_SD_prompt] * num_out_imgs).images    # Concept art
+    correct_mood = "CORRECT" if (set(pred_moods) & set(GT_moods)) else "INCORRECT"
+    correct_genre = "CORRECT" if (set(pred_genres) & set(GT_genres)) else "INCORRECT"
+    print("Mood: ", correct_mood, " (GT: ", GT_moods, ", Pred mood: ", pred_moods, ")")
+    print("Genre: ", correct_genre, " (GT: ", GT_genres, ", Pred genre: ", pred_genres, ")\n")
     
-    # Dump outputs
-    out_dir = "./CPSC532S_FinalProject/outputs/inference/"
-    info_dict = {
-        'URL': vid_url,
-        'GT_mood': moods[mood_idx],
-        'GT_genre': genres[genre_idx],
-        'pred_mood': moods[pred_mood_idx], 
-        'pred_genre': genres[pred_genre_idx],
-        'album_prompt': SD_prompt,
-        'concept_prompt': extended_SD_prompt,
-    }
-    info_json = json.dumps(info_dict)
-    with open(out_dir + "info.json", "w") as outfile:
-        outfile.write(info_json)
-    for i, img in enumerate(SD_images_album):
-        img.save(out_dir + "album_output_" + str(i) + ".png")
-    for i, img in enumerate(SD_images_concept):
-        img.save(out_dir + "concept_output_" + str(i) + ".png")
+    # # Create genre-based prompt
+    # prompt_mood = pred_moods[0]     # TEMP: just take the top predictions
+    # prompt_genre = pred_genres[0]
+    # SD_prompt = prompt_mood + " " + prompt_genre + " music album cover"
+    # print("Input prompt: ", SD_prompt, "\n")
+    # text_pipe = pipeline('text-generation', model='daspartho/prompt-extend', device=0)
+    # extended_SD_prompt = text_pipe(SD_prompt+',', num_return_sequences=1, max_length=25)[0]["generated_text"]
+    # extended_SD_prompt
+    # print("Extended input prompt: ", extended_SD_prompt, "\n")
+
+    # # Generate output imgs with SD
+    # num_out_imgs = 2
+    # SD_model_id = "stabilityai/stable-diffusion-2"
+    # scheduler = EulerDiscreteScheduler.from_pretrained(SD_model_id, subfolder="scheduler")
+    # pipe = StableDiffusionPipeline.from_pretrained(SD_model_id, scheduler=scheduler, torch_dtype=torch.float16, revision="fp16")
+    # pipe = pipe.to("cuda")
+    # pipe.enable_attention_slicing() # Reduce GPU usage
+    # SD_images_album = pipe([SD_prompt] * num_out_imgs).images             # Album cover
+    # SD_images_concept = pipe([extended_SD_prompt] * num_out_imgs).images    # Concept art
+    
+    # # Dump outputs
+    # out_dir = "./CPSC532S_FinalProject/outputs/inference/"
+    # info_dict = {
+    #     'URL': vid_url,
+    #     'GT_moods': GT_moods,
+    #     'GT_genres': GT_genres,
+    #     'pred_moods': pred_moods, 
+    #     'pred_genres': pred_genres,
+    #     'album_prompt': SD_prompt,
+    #     'concept_prompt': extended_SD_prompt,
+    # }
+    # info_json = json.dumps(info_dict)
+    # with open(out_dir + "info.json", "w") as outfile:
+    #     outfile.write(info_json)
+    # for i, img in enumerate(SD_images_album):
+    #     img.save(out_dir + "album_output_" + str(i) + ".png")
+    # for i, img in enumerate(SD_images_concept):
+    #     img.save(out_dir + "concept_output_" + str(i) + ".png")
 
 if __name__ == "__main__":
     main()
